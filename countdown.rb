feb12b3d4d5c9dff5a09ea40686106ccedea85b6
David Thayer <dthayer16@gmail.com>
2019-02-13 21:42:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,7 @@ def countdown (number_of_times)
   count = 0
   while count < number_of_times
     puts "#{number_of_times} SECOND(S)!"
+    sleep 5
     number_of_times -= 1
     end
   return "HAPPY NEW YEAR!"
