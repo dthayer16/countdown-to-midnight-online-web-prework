dfe6b5bc05cc3c3ca1b088287e7ffa546fd36de7
David Thayer <dthayer16@gmail.com>
2019-02-13 21:49:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown (number_of_times)
+  count = 0
+  while count < number_of_times
+    puts "#{number_of_times} SECOND(S)!"
+    number_of_times -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (number_of_times)
+  count = 0
+  while count < number_of_times
+    puts "#{number_of_times} SECOND(S)!"
+    sleep (1)
+    number_of_times -= 1
+    end
+  return "HAPPY NEW YEAR!"
 end
