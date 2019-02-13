c66284777d31702a4fe90ca963766c7580effdff
David Thayer <dthayer16@gmail.com>
2019-02-13 21:11:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
-  time = 10
+  
 def countdown
-  while time > 0
+  time = 10
+  while time > 1
     puts "#{time} SECONDS!"
-  time -= 1
+    time -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
