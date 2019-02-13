691c25f6f603f17fb228f5012fb1ae36497b5921
David Thayer <dthayer16@gmail.com>
2019-02-13 21:38:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 def countdown (number_of_times)
   count = 0
   while count < number_of_times
-    puts "{count} SECOND(S)!"
+    puts "{number_of_times} SECOND(S)!"
     number_of_times -= 1
     end
   puts "HAPPY NEW YEAR!"
