936d98a788d17b226c9c37fe5eb48d50349ce3ff
David Thayer <dthayer16@gmail.com>
2019-02-13 21:20:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 def countdown
-  count = 10
-  while count > 0
+  count = 0
+  while count > 10
     puts "#{count} SECONDS!"
       sleep (5)
-    count -= 1
+    count += 1
     end
   puts "HAPPY NEW YEAR!"
 end
